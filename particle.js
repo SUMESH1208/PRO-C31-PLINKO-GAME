987bc89a6095debe0d8813c336e3a2abe56e0bfc
SUMESH URC <80336511+SUMESH1208@users.noreply.github.com>
2021-09-08 08:51:57 +0530

    Update particle.js

--- particle.js
+++ particle.js
@@ -5,7 +5,7 @@ class Particle{
             
         }
         this.r = r
-        this.body = Bodies.rectangle(x, y, width,this.r, options);
+        this.body = Bodies.circle(x, y, this.r, options);
         this.color = color(random(0,255),random(0,255), random(0,255))
         World.add(world, this.body);
       }
